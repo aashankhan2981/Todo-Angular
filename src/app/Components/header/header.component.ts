2b0ebab9b03ebc535dd1995e137402eb9c7e27be
aashankhan2981 <aashankhan2981@gmail.com>
2021-12-30 01:03:42 +0500

    responsive

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  toggle:boolean=false
   constructor() { }
-
+  
   ngOnInit(): void {
   }
-
-}
+  Menu(){
+   this.toggle = !this.toggle
+  }
+} 
